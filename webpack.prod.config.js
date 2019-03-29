0d1735f16ba4dbce7b314fe4c8c4d9b3f3cca6c4
Buddha Man Nepali <bmnepali980@gmail.com>
2019-03-29 21:29:28 +0545

    [config] RRS: Re-adds webpack dev and prod config files

---
+++ webpack.prod.config.js
@@ -0,0 +1,83 @@
+const webpack = require('webpack');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  entry: ['./src/index.js', './src/styles/style.scss'],
+  performance: {
+    // hints: false,
+    // maxEntrypointSize: 512000,
+    // maxAssetSize: 512000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader, options: { publicPath: './src/styles/', hmr: false } },
+          { loader: 'css-loader', options: { url: false, sourceMap: false } },
+          { loader: 'sass-loader', options: { sourceMap: false, implementation: require('node-sass') } },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/images/[name].[ext]',
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            // Using file-loader too
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.min.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: 'style.min.css' }),
+    new HtmlWebpackPlugin({
+      title: 'React Redux Starter Project',
+      template: './src/index.tpl.html',
+      inject: 'body',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
+};
