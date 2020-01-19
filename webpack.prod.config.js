63f935cbe8fd3b38ed2414ee9aef82ffcc463d3f
Buddha Man Nepali <bmnepali980@gmail.com>
2020-01-19 17:15:04 +0545

    [config] RRS: Updates production webpack config file.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -6,13 +6,13 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'prod',
   devtool: false,
   entry: ['./src/index.js', './src/styles/style.scss'],
   performance: {
-    // hints: false,
-    // maxEntrypointSize: 512000,
-    // maxAssetSize: 512000
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
   module: {
     rules: [
